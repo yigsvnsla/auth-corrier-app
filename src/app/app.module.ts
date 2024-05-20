83eb2e7e74b0a3af00ef3fe58f0270be273f4eca
Jesus Guzman <jesus.guzman@integritysolutions.com.ec>
2024-05-20 17:26:17 -0500

    add : keycloack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OAuth2Strategy } from './auth/Auth2.strategy';
 
 @Module({
 	imports: [],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, OAuth2Strategy],
 })
 export class AppModule {}
--- src/app/auth/Auth2.strategy.ts
+++ src/app/auth/Auth2.strategy.ts
@@ -6,6 +6,12 @@ import { AppService } from '../app.service';
 @Injectable()
 export class OAuth2Strategy extends PassportStrategy(Strategy) {
 	constructor(private appService: AppService) {
-		super();
+		super({
+			authorizationURL: 'https://www.example.com/oauth2/authorize',
+			tokenURL: 'https://www.example.com/oauth2/token',
+			clientID: 'nestjs-app',
+			clientSecret: 'MGuws0NYlyOh6pnbDeCHbDM2GlRdN2iY',
+			callbackURL: 'http://localhost:3000/auth/example/callback',
+		});
 	}
 }
